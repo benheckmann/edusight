2d6d29c0957d1c0ea8bb8a0f089de90006efdcb9
Ben Heckmann <ben.c.heckmann@gmail.com>
2023-09-01 14:48:35 +0200

    implement simple working API & DB connection

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import NavigationBar from "../components/NavigationBar";
 import IntroHeader from '../sections/IntroHeader';
 import { YellowBannerOurMission, YellowBannerWhyEdusight } from "../sections/YellowBanner"
@@ -6,10 +7,37 @@ import Footer from '../sections/Footer';
 import '../App.css';
 
 function LandingPage() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:5000/hello-world')
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Error: ' + response.status);
+                }
+            })
+            .then(data => {
+                setData(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error.message);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Errorsdfsdf: {error}</div>;
+
   return (
     <>
       <NavigationBar />
       <IntroHeader />
+      {data && <div>{JSON.stringify(data)}</div>}
       <YellowBannerOurMission />
       <Walkthrough />
       <YellowBannerWhyEdusight />
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
 from flask_login import (
     LoginManager,
@@ -17,18 +17,20 @@ import os
 load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:5000"]}})
 
 # configuration
-app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+mongo_uri = os.getenv("MONGO_URI")
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
+# Initialize MongoDB client and database
+client = MongoClient(mongo_uri)
+db = client['general']
+
 # extensions
-mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
-
 
 class User(UserMixin):
     def __init__(self, email, password, _id=None):
@@ -36,20 +38,21 @@ class User(UserMixin):
         self.email = email
         self.password = password
 
-
 @login_manager.user_loader
 def load_user(user_id):
-    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    user = db.users.find_one({"_id": ObjectId(user_id)})
     if not user:
         return None
     return User(user["email"], user["password"], str(user["_id"]))
 
-
-@app.route("/")
-@login_required
+@app.route("/hello-world")
 def hello_world():
     return jsonify(message="Hello, World!")
 
+@app.route("/test-db")
+def test_db():
+    collections = db.list_collection_names()
+    return jsonify({"collections": collections})
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -57,25 +60,21 @@ def register():
     password = request.json.get("password", None)
     hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
 
-    mongo.db.users.insert_one({"email": email, "password": hashed_password})
-
+    db.users.insert_one({"email": email, "password": hashed_password})
     return jsonify(message="User registered."), 201
-
 
 @app.route("/login", methods=["POST"])
 def login():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-    user = mongo.db.users.find_one({"email": email})
+    user = db.users.find_one({"email": email})
 
     if not user or not bcrypt.check_password_hash(user["password"], password):
         return jsonify(message="Invalid email or password."), 401
 
     user_obj = User(user["email"], user["password"], str(user["_id"]))
     login_user(user_obj)
-
     return jsonify(message="Logged in successfully."), 200
-
 
 @app.route("/logout")
 @login_required
@@ -83,21 +82,19 @@ def logout():
     logout_user()
     return jsonify(message="Logged out successfully."), 200
 
-
 @app.route("/reset_password", methods=["POST"])
 @login_required
 def reset_password():
     old_password = request.json.get("old_password", None)
     new_password = request.json.get("new_password", None)
-    user = mongo.db.users.find_one({"_id": ObjectId(current_user.get_id())})
+    user = db.users.find_one({"_id": ObjectId(current_user.get_id())})
 
     if not bcrypt.check_password_hash(user["password"], old_password):
         return jsonify(message="Old password is incorrect."), 401
 
     hashed_new_password = bcrypt.generate_password_hash(new_password).decode("utf8")
-    mongo.db.users.update_one(
+    db.users.update_one(
         {"_id": ObjectId(current_user.get_id())},
         {"$set": {"password": hashed_new_password}},
     )
-
     return jsonify(message="Password reset successful."), 200
